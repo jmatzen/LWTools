80fa95c0c5fe900dcb85cee516fe6a03cef344c1
lost <lost@l-w.ca>
2011-08-29 19:05:18 -0600

    Make multiple instances of constant sections all start at offset 0 rather than merging them

--- lwasm/section.c
+++ lwasm/section.c
@@ -138,6 +138,9 @@ PARSEFUNC(pseudo_parse_section)
 		as -> sections = s;
 	}
 	
+	// cause all instances of "constant" sections to start at 0
+	if (s -> flags & section_flag_constant)
+		s -> offset = 0;
 	lw_expr_destroy(l -> addr);
 	l -> addr = lw_expr_copy(s -> offset);
 	lw_expr_destroy(l -> daddr);
